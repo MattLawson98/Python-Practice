812e9f99658f64f414f4581e733392f5dc79b640
MattLawson98 <matthewlawson50@gmail.com>
2022-10-06 13:36:16 -0700

    Defined the left and right click actions

--- mineSweeper/cell.py
+++ mineSweeper/cell.py
@@ -9,5 +9,15 @@ class Cell:
       location,
       text='Text'
     )
+    btn.bind('<Button-1>',self.left_click_actions)#Left click
+    btn.bind('<Button-3>',self.right_click_actions)#Right click
     self.cell_btn_object = btn
     
+  def left_click_actions(self, event):
+    print(event)
+    print("I am left clicked!") 
+  
+  def right_click_actions(self, event):
+    print(event)
+    print("I am right clicked!") 
+     
